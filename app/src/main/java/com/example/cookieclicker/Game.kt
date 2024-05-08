59e608997e2634dd7f4d5ba77d2bd1673ec761cb
adamGIT <106454017+adamGITTY@users.noreply.github.com>
2024-05-07 22:58:49 -0400

    Add files via upload

--- app/src/main/java/com/example/cookieclicker/Game.kt
+++ app/src/main/java/com/example/cookieclicker/Game.kt
@@ -3,7 +3,6 @@ package com.example.cookieclicker
 class Game(cookies: Int, multiplier: Int) {
     private var cookies = cookies
     private var multiplier = multiplier
-    private lateinit var upgrades : Map<UpgradeType, Int>
     fun getCookies() : Int {
         return cookies
     }
@@ -12,11 +11,7 @@ class Game(cookies: Int, multiplier: Int) {
         this.cookies += multiplier
     }
 
-    fun getUpgrades() : Map<UpgradeType, Int> {
-        return upgrades
-    }
-
-    fun setUpgrades(upgrades : Map<UpgradeType, Int>) {
-        this.upgrades = upgrades
+    fun jackpot() {
+        this.cookies += 99 * multiplier
     }
 }
